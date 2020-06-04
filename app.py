3b591d889024e628bdc7e4a84ce900d0d3e0af0e
Carter Tat <carter14@vt.edu>
2020-06-03 20:44:53 -0400

    Changed naming to app.py

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask, json
 
 companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
 
-api = Flask(__name__)
+app = Flask(__name__)
 
-@api.route('/companies', methods=['GET'])
+@app.route('/companies', methods=['GET'])
 def get_companies():
   return json.dumps(companies)
 
-@api.route('/companies', methods=['POST'])
+@app.route('/companies', methods=['POST'])
 def post_companies():
   return json.dumps({"success": True}), 201
 
 if __name__ == '__main__':
-    api.run()
+    app.run()
