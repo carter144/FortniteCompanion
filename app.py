a36885e26930b8a8ced77863a2b311c5920ccab3
levane <levanedn@gmail.com>
2020-06-05 20:34:26 -0400

    WIP: seeing what's inside message

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def handleMessage(sender_psid, received_message):
             conversations.addId(sender_psid)
             requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + os.getenv("page_token"), json=request_body)
     elif "text" in received_message:
-        print(received_message["text"])
+        print(received_message)
     else:
         request_body = {
             "recipient": {"id": sender_psid},
